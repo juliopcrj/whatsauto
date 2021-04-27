a6bdfa5a03ed44f8397eb22c66900fb97217d230
Julio Pinto Coelho <juliopcrj@gmail.com>
2021-04-27 16:56:12 -0300

    Usando sys.argv pra indicar arquivo de contatos e mensagens

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import sys
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,35 +6,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument("--user-data-dir=./User_Data")
-driver = webdriver.Chrome(chrome_options=options)
-
-contacts = open("contacts.csv").read()[:-1].split("\n")
-message = open("message.csv").read()[:-1].split("\n")[0]
-
-timeout = 10
 
 #[attempt 1]
-for c in contacts:
-    driver.get("https://web.whatsapp.com/send?phone=" + c + "&text="+ message)
-    try:
-        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'OTBsx'))
-        WebDriverWait(driver, timeout).until(element_present)
-        button = driver.find_element_by_xpath('//button[@class="_1E0Oz"]')
-        button.click()
-        continue
-    except:
-        print("Page took too long to load")
-
+#timeout = 10
+#for c in contacts:
+#    driver.get("https://web.whatsapp.com/send?phone=" + c + "&text="+ message)
+#    element_present = EC.presence_of_element_located((By.CLASS_NAME, 'OTBsx'))
+#    WebDriverWait(driver, timeout).until(element_present)
+#    button = driver.find_element_by_xpath('//button[@class="_1E0Oz"]')
+#    button.click()
+    
 
 #[attempt 2]
+def mass_send(contacts, message):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--user-data-dir=./User_Data")
+    driver = webdriver.Chrome(chrome_options=options)
 
-for c in contacts:
-    driver.get("https://web.whatsapp.com/send?phone=" + c + "&text=" + message)
-    try:
-        time.sleep(10)
-        button = driver.find_element_by_xpath('//button[@class="_1E0Oz"]')
-        button.click()
-    except:
-        print("Error")
+    for c in contacts:
+        driver.get("https://web.whatsapp.com/send?phone=" + c + "&text=" + message)
+        try:
+            time.sleep(10)
+            button = driver.find_element_by_xpath('//button[@class="_1E0Oz"]')
+            button.click()
+            time.sleep(10)
+        except:
+            print("Error")
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 3):
+        print("Forneça os arquivos de contatos e mensagem.\nO uso correto do programa é \npython "+sys.argv[0] + " arquivo_contato arquivo_mensagem")
+        sys.exit(0)
+    contacts = open(sys.argv[1]).read()[:-1].split("\n")
+    message = open(sys.argv[2]).read()[:-1]
+
+    mass_send(contacts, message)
