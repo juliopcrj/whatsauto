7ad794b6d0e805b2df07c12c2e677ced0bab7e84
Julio Pinto Coelho <juliopcrj@gmail.com>
2021-05-19 09:26:02 -0300

    Encapsulando exceção em try/except

--- script.py
+++ script.py
@@ -10,16 +10,23 @@ def mass_send(driver, contacts, message):
 
     for c in contacts:
         search_field.click()
+        search_field.send_keys(Keys.CONTROL, 'a')
+        search_field.send_keys(Keys.BACKSPACE)
         search_field.send_keys(c)
         time.sleep(2)
-        search_field.send_keys(Keys.DOWN)
-        search_field.send_keys(Keys.RETURN)
-        box = driver.switch_to.active_element
-        box.send_keys(message)
-        box.send_keys(Keys.RETURN)
-        with open("log_day.txt", "a+") as f:
-            f.write("Enviou para " + c + "\n")
-        time.sleep(2)
+        try:
+            search_field.send_keys(Keys.DOWN)
+            search_field.send_keys(Keys.RETURN)
+            box = driver.switch_to.active_element
+            box.send_keys(message)
+            box.send_keys(Keys.RETURN)
+            with open("log_day.txt", "a+") as f:
+                f.write("Enviou para " + c + "\n")
+            time.sleep(2)
+        except Exception:
+            with open("log_error.txt", "a+") as f:
+                f.write("Erro enviando para " + c +"\n")
+            time.sleep(2)
 
 
 if __name__ == "__main__":
@@ -31,6 +38,7 @@ if __name__ == "__main__":
     options.add_argument("--user-data-dir="+ os.path.join(".","User_Data"))
     driver = wd.Chrome(options=options)
     contacts = open(sys.argv[1]).read().split("\n")
+    contacts = [c for c in contacts if c != ""] #gambiarra para remover linhas vazias.
     message = open(sys.argv[2], encoding="utf-8").read()
     sys.exit(mass_send(driver, contacts, message))
 
